9781e6e67cf4bed24687995cdd372faf828783b5
LudGalvao <ludgalvao09@gmail.com>
2025-03-20 10:50:06 -0300

    [Initial Commit] Initial commit for SpringBoot-NFlash project

---
+++ src/test/java/com/nflash/nfespring/NfespringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nflash.nfespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NfespringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
