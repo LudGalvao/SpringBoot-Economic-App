8d7dfb9e0474d0d39c38f8b045498db3c718f6d4
LudGalvao <ludgalvao09@gmail.com>
2025-03-28 17:03:37 -0300

    [New] - Criação do usuario.java

---
+++ src/main/java/com/nflash/nfespring/entity/Usuario.java
@@ -0,0 +1,67 @@
+package com.nflash.nfespring.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "usuario")
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(columnDefinition = "INT", name = "id_usuario", nullable = false)
+    private int id;
+
+    @Column(columnDefinition = "VARCHAR", length = 64, name = "username", nullable = false)
+    private String nome;
+
+    @Column(columnDefinition = "VARCHAR", length = 255, name = "email", nullable = false)
+    private String email;
+
+    @Column(columnDefinition = "VARCHAR", length = 255, name = "senha_hash", nullable = false)
+    private String senha;
+
+    @Column(columnDefinition = "VARCHAR" , length = 14, name = "cpf", nullable = false)
+    private int cpf;
+
+    @ManyToOne
+    @JoinColumn(name = "tipo_plano_id")
+    private Plano plano;
+
+    @Column(columnDefinition = "TIMESTAMP", name = "data_cadastro")
+    private LocalDateTime dataCadastro;
+
+    @Column(columnDefinition = "TIMESTAMP", name = "ultimo_login")
+    private LocalDateTime ultimoLogin;
+
+    @Column(columnDefinition = "TINYINT(1)", name = "ativo")
+    private boolean ativo;
+
+    @Column(columnDefinition = "VARCHAR", length = 255, name = "razao_social")
+    private String razaoSocial;
+
+    @Column(columnDefinition = "VARCHAR", length = 255, name = "nome_fantasia")
+    private String nomeFantasia;
+
+    @Column(columnDefinition = "VARCHAR", length = 20, name = "telefone")
+    private String telefone;
+
+    @Column(columnDefinition = "JSON", name = "endereco")
+    private String endereco;
+}
