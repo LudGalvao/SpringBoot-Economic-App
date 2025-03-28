57b16940d616de120fa7bf46bc57c9a7ac03a79b
LudGalvao <ludgalvao09@gmail.com>
2025-03-28 17:02:19 -0300

    [Update] - add anotações para chave estrageira

--- src/main/java/com/nflash/nfespring/entity/Cliente.java
+++ src/main/java/com/nflash/nfespring/entity/Cliente.java
@@ -7,6 +7,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -49,4 +51,8 @@ public class Cliente {
 
     @Column(columnDefinition = "TINYINT(1)", name = "ativo")
     private boolean ativo;
+
+    @ManyToOne
+    @JoinColumn(name = "id_usuario")
+    private Usuario usuario;
 }
