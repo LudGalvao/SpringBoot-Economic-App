eabfb186b6992029c6794d62cf7a2325d9489c91
LudGalvao <ludgalvao09@gmail.com>
2025-05-05 19:47:28 -0300

    add arquivos de configurações

---
+++ src/main/java/com/economic/app/economic_app/EconomicAppApplication.java
@@ -0,0 +1,13 @@
+package com.economic.app.economic_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EconomicAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EconomicAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/economic/app/economic_app/EconomicAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.economic.app.economic_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EconomicAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
