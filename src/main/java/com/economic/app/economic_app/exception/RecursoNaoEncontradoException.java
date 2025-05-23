411dd53ba94534da3845d3bbc1526a689fdeefc6
LudGalvao <ludgalvao09@gmail.com>
2025-05-23 20:48:55 -0300

    update

--- src/main/java/com/economic/app/economic_app/exception/RecursoNaoEncontradoException.java
+++ src/main/java/com/economic/app/economic_app/exception/RecursoNaoEncontradoException.java
@@ -6,7 +6,13 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class RecursoNaoEncontradoException extends RuntimeException {
     
+    private static final long serialVersionUID = 1L;
+    
     public RecursoNaoEncontradoException(String mensagem) {
         super(mensagem);
+    }
+    
+    public RecursoNaoEncontradoException(String mensagem, Throwable causa) {
+        super(mensagem, causa);
     }
 } 
