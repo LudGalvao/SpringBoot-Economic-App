636a2f6dd129cbbc02b54dc3798ff1b5d7f60d58
LudGalvao <ludgalvao09@gmail.com>
2025-05-23 20:45:44 -0300

    fix: restaura configuração do MongoDB em application.properties
    Reverte as configurações de H2 para MongoDB e restaura as configurações originais
    incluindo a URL de conexão e a exclusão de configurações automáticas de JPA.

--- src/main/java/com/economic/app/economic_app/EconomicAppApplication.java
+++ src/main/java/com/economic/app/economic_app/EconomicAppApplication.java
@@ -2,16 +2,21 @@ package com.economic.app.economic_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+import com.economic.app.economic_app.config.AppProperties;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackages = "com.economic.app.economic_app.repository")
+@EnableConfigurationProperties(AppProperties.class)
 public class EconomicAppApplication {
 
 	public static void main(String[] args) {
